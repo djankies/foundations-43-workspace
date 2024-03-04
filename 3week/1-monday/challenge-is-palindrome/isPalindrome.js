8619e7d3b0e74f158fbbce9101a360828426d69f
Daniel Jankowski <djank0113@gmail.com>
2024-03-04 11:10:09 -0500

    Finished problem

--- 3week/1-monday/challenge-is-palindrome/isPalindrome.js
+++ 3week/1-monday/challenge-is-palindrome/isPalindrome.js
@@ -1,1 +1,9 @@
 // Write your code below
+function isPalindrome(word) {
+    revWord = word.split('').reverse().join('');
+    return word === revWord;
+}
+
+let result = isPalindrome('hello')
+
+console.log(result);
