6f0f7409995061c165d2ecac81757308d9d4c324
Daniel Jankowski <djank0113@gmail.com>
2024-03-08 11:43:39 -0500

    Review Work

---
+++ 3week/5 - friday/index.js
@@ -0,0 +1,18 @@
+let pizza1 = document.querySelector('#pizza-1')
+let pizza2 = document.querySelector('#pizza-2')
+let pizza3 = document.querySelector('#pizza-3')
+
+const pepperoni = () => {
+    alert('pizza')
+}
+const bbq = () => {
+    alert('another')
+}
+const hawaiian = () => {
+    alert('haha')
+}
+
+pizza1.addEventListener('mouseover', pepperoni)
+pizza2.addEventListener('mouseover', bbq)
+pizza3.addEventListener
+
