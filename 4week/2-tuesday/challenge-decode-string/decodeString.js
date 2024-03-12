3ce1e1b6b057978a47e5a0ed65ce538d596640ee
Daniel Jankowski <djank0113@gmail.com>
2024-03-12 12:09:23 -0400

    Finished Challenge

--- 4week/2-tuesday/challenge-decode-string/decodeString.js
+++ 4week/2-tuesday/challenge-decode-string/decodeString.js
@@ -1,1 +1,18 @@
 // Write your code below this line
+
+function decodeString(str) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let decodedString = [];
+    let encodedString = [];
+    let strArr = str.split("");
+    let num = strArr.shift();
+    console.log(strArr);
+    strArr.forEach((letter) => {
+        console.log(letter);
+
+        let newLetter = alphabet[alphabet.indexOf(letter) + +num];
+        decodedString.push(newLetter);
+    });
+    return decodedString.join("");
+}
+console.log(decodeString("2abcdefg"));
