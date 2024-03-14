ac96f87577cb0b6ff149d06e3381ee924b1177b2
Daniel Jankowski <djank0113@gmail.com>
2024-03-14 11:21:55 -0400

    Solution

--- 4week/4-thursday/chal-balanced-parenthesis/balancedParens.js
+++ 4week/4-thursday/chal-balanced-parenthesis/balancedParens.js
@@ -5,3 +5,45 @@ let sample3 = "This is () also ) unbalanced."
 let sample4 = "Balanced."
 
 // Write your solution below:
+
+function balanceString (str) {
+    let leftCount = 0
+    
+    let rightCount = 0
+    
+    let strArr = str.split('')
+    
+    strArr.forEach(character => {
+        if (character === '\(') {
+            leftCount++
+        } else if (character === '\)'){
+            rightCount++
+        }
+    })
+    if (leftCount === rightCount) {
+        return true
+    } else {
+        return false
+    }
+
+    // while(leftCount !== rightCount) {
+    //     if (leftCount > rightCount) {
+    //             strArr.push("\)")
+    //             rightCount++
+    //         } else if (rightCount > leftCount) {
+    //             strArr.push("\(" );
+    //             leftCount++
+    //         }
+    //     }
+    // return strArr.join('')
+}
+
+console.log(balanceString("This ( is unbalanced.)"));
+console.log(
+    "balanceString (\"This () is balanced. \"):", 
+    balanceString("This \( is unbalanced.)"));
+
+console.log(
+    'balanceString ("This ( is unbalanced. "):',
+    balanceString("This ( is unbalanced.")
+);
